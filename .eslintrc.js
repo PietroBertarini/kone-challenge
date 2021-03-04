682f42409f802306836f9ba968ee6ace1cea273c
Pietro <pietromota@hotmail.com>
2021-03-04 13:10:00 -0300

    Initial comiit
    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    jasmine: true,
+
+  },
+  extends: [
+    'react-app',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', 'jsx-a11y'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'no-console': ['warn', { allow: ['warn'] }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'react/require-default-props': 'off',
+    'import/no-named-as-default': 0,
+    'no-use-before-define': 'off',
+    'no-param-reassign': 0,
+    'max-len': [2, { code: 120, ignoreUrls: true }],
+    'linebreak-style': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
