930e741f490eb00622d6a82d30e34e1e780081db
Pietro <pietromota@hotmail.com>
2021-03-06 18:17:57 -0300

    Added upload .csv files.

--- src/Components/LegoComponent/ModalBodys/UploadData/UploadData.tsx
+++ src/Components/LegoComponent/ModalBodys/UploadData/UploadData.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
+import { CSVReader } from 'react-papaparse';
 import {
   BodyHeader, ButtonText, FileButton, FlexContainer, Subtitle,
 } from './UploadData.styles';
 
+const buttonRef : any = React.createRef();
 const UploadData = () => {
-  const portalDiv = document.getElementById('portal');
+  function handleOpenDialog(e: any) {
+    // Note that the ref is set async, so it might be null at some point
+    if (buttonRef.current) {
+      buttonRef.current.open(e);
+    }
+  };
+
+  function handleOnFileLoad(data: any, file: any) {
+    console.log(file.name);
+    console.log('---------------------------');
+    console.log(data);
+    console.log('---------------------------');
+  };
+
+  function handleOnError(err : any, file: any, inputElem : any, reason : any) {
+    console.log('---------------------------');
+    console.log(err);
+    console.log('---------------------------');
+  };
+
   return (
     <FlexContainer>
       <BodyHeader>Upload Data</BodyHeader>
-      <FileButton>
-        <ButtonText>
-          Select File
-        </ButtonText>
-      </FileButton>
+      <CSVReader
+        ref={buttonRef}
+        onFileLoad={handleOnFileLoad}
+        onError={handleOnError}
+        noDrag
+        progressBarColor="#2F54D0"
+      >
+        {() => (
+          <aside
+            style={{
+              cursor: 'pointer',
+            }}
+          >
+            <FileButton>
+              <ButtonText onClick={handleOpenDialog}>
+                Select File
+              </ButtonText>
+            </FileButton>
+          </aside>
+        )}
+      </CSVReader>
       <Subtitle>File must be .csv format</Subtitle>
     </FlexContainer>
   );
